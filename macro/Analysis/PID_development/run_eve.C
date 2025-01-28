c1cf6d3e70a8ce0717271fb7c09632b555eae892
anthoak13 <aanthon2@highpoint.edu>
2025-01-28 13:10:59 -0500

    Add josh scripts

---
+++ macro/Analysis/PID_development/run_eve.C
@@ -0,0 +1,39 @@
+void run_eve(TString InputDataFileName = "run_0053")
+{
+
+   TString InputDataFile = "data/" + InputDataFileName + ".root";
+
+   TString OutputDataFile = InputDataFileName + ".reco_display.root";
+
+   TString dir = getenv("VMCWORKDIR");
+   TString geoFile = "ATTPC_He1bar_v2_geomanager.root";
+   TString mapFile = "e12014_pad_mapping.xml";
+
+   TString InputDataPath = InputDataFile;
+   TString OutputDataPath = OutputDataFile;
+   TString GeoDataPath = dir + "/geometry/" + geoFile;
+   TString mapDir = dir + "/scripts/" + mapFile;
+
+   FairRunAna *fRun = new FairRunAna();
+   FairRootFileSink *sink = new FairRootFileSink(OutputDataFile);
+   FairFileSource *source = new FairFileSource(InputDataFile);
+   fRun->SetSource(source);
+   fRun->SetSink(sink);
+   fRun->SetGeomFile(GeoDataPath);
+
+   FairRuntimeDb *rtdb = fRun->GetRuntimeDb();
+   FairParRootFileIo *parIo1 = new FairParRootFileIo();
+   // parIo1->open("param.dummy.root");
+   rtdb->setFirstInput(parIo1);
+
+   auto fMap = std::make_shared<AtTpcMap>();
+   fMap->ParseXMLMap(mapDir.Data());
+
+  AtViewerManager *eveMan = new AtViewerManager(fMap);
+
+   auto tabMain = std::make_unique<AtTabMain>();
+   tabMain->SetMultiHit(100); // Set the maximum number of multihits in the visualization
+   eveMan->AddTab(std::move(tabMain));
+
+   eveMan->Init();
+}
